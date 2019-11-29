e007e2974cdcf83e66c5d3b0779e65feb2730717
Less <less1226@users.noreply.github.com>
2019-11-29 16:13:25 +0800

    Create 387.firstUniqChar.py

---
+++ leetcode/387.firstUniqChar.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        for i in range(len(s)):
+            if s[i] in dic:
+                dic[s[i]] = -1
+            else:
+                dic[s[i]] = i
+        
+        li = [x for i,x in dic.items() if x != -1]
+        li.sort()
+        return li[0] if len(li) >0 else -1
+        
