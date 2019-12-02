390a4e353fdb59c86ea8516e4725ebbe7cd8ce2e
Less <less1226@users.noreply.github.com>
2019-12-02 13:42:47 +0800

    Create 242.isAnagram.py

---
+++ leetcode/242.isAnagram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dic_s={}
+        dic_t={}
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            dic_s[s[i]] = dic_s[s[i]] +1 if s[i] in dic_s else 1
+            dic_t[t[i]] = dic_t[t[i]] +1 if t[i] in dic_t else 1
+            
+        for key, item in dic_t.items():
+            if key not in dic_s:
+                return False
+            elif item != dic_s[key]:
+                return False
+        
+        return True
