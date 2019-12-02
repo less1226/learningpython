7fd261067f653d764bc3aa3f4d34f38a84316f7f
Less <less1226@users.noreply.github.com>
2019-12-02 13:47:31 +0800

    Create 125.isPalindrome.py

---
+++ leetcode/125.isPalindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s)-1
+        while start <end:
+            if s[start].isalnum() and s[end].isalnum():
+                if s[start].lower() == s[end].lower():
+                    start += 1
+                    end -= 1
+                else:
+                    return False
+            if s[start].isalnum() == False:
+                start +=1
+            if s[end].isalnum() == False:
+                end -=1
+        
+        return True
